0851c49c849ea1af247ea3bcc3e271836b925892
drejohnson <drejohnson212@gmail.com>
2020-01-07 16:39:14 +0800

    rename

--- middleware/verifyToken.mjs
+++ middleware/verifyToken.mjs
@@ -1,16 +1,16 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 
-const verify = (req, res, next) => {
-  const token = req.headers["authorization"];
-  if (!token) return res.status(401).send("Access Denied");
+const verifyToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+  if (!token) return res.status(401).send('Access Denied');
 
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
     next();
   } catch (error) {
-    res.status(400).send("Invalid Token");
+    res.status(400).send('Invalid Token');
   }
 };
 
-export default verify;
+export default verifyToken;
