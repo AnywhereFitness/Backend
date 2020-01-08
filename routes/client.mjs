ba44c4645eadf18c65255e83e581da0f49a05658
drejohnson <drejohnson212@gmail.com>
2020-01-08 13:25:38 +0800

    added routes for client

---
+++ routes/client.mjs
@@ -0,0 +1,82 @@
+import express from 'express';
+import verifyToken from '../middleware/verifyToken.mjs';
+import verifyRole from '../middleware/verifyRole.mjs';
+import Class from '../model/Class.mjs';
+import Reservation from '../model/Reservation.mjs';
+import User from '../model/User.mjs';
+
+const router = express.Router();
+
+// Get Client Users
+router.get(
+  '/profile',
+  [verifyToken, verifyRole('client')],
+  async (req, res) => {
+    try {
+      const profile = await User.findOne({ _id: req.user.id });
+      const reservations = await Reservation.find({
+        userId: req.user.id
+      }).select('_id classId');
+      const { _id, firstName, lastName, email } = profile;
+      res.send({ _id, firstName, lastName, email, reservations });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+);
+
+// Get Reservations
+router.get(
+  '/reservations',
+  [verifyToken, verifyRole('client')],
+  async (req, res) => {
+    try {
+      const reservations = await Reservation.find({
+        userId: req.user.id
+      }).select('userId classId');
+      res.send(reservations);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+);
+
+// Add Reservation
+router.post(
+  '/reservation/:classId',
+  [verifyToken, verifyRole('client')],
+  async (req, res) => {
+    const { classId } = req.params;
+
+    try {
+      const reservation = new Reservation({
+        classId: classId,
+        userId: req.user.id
+      });
+      const newReservation = await reservation.save();
+
+      res.send(newReservation);
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+);
+
+// Delete Reservation
+router.delete(
+  '/reservation/:classId',
+  [verifyToken, verifyRole('client')],
+  async (req, res) => {
+    const { classId } = req.params;
+    try {
+      const removedReservation = await Reservation.deleteOne({ classId });
+      res.send({
+        message: `${removedReservation.deletedCount} Reservation deleted`
+      });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  }
+);
+
+export default router;
